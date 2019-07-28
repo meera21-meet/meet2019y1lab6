68a0ef4c80f0fb1e7440acbd0d98fdea731d40f3
meera21-meet <meera21@meet.mit.edu>
2019-07-28 09:32:07 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,16 @@
+# add the numbers
+#total = 0
+#for number in range (1,10 + 1):
+    #print (number)
+    #total= total+number
+#print (total)
+
+def add_numbers():
+    total = 0
+    for number in range (1,11):
+         print (number)
+         total = total+number
+    return total
+answer =add_numbers()
+print(answer)
+           
